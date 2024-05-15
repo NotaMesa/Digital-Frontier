9858fc6683be6c334cada5f9a4b987e051fbad53
Dave Butte <47307082+NotaMesa@users.noreply.github.com>
2024-05-14 21:18:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/main.css">
+    <link rel="stylesheet" href="assets/style/bg-main.css">
     <script src="https://kit.fontawesome.com/fa41d7c648.js" crossorigin="anonymous"></script>
     <title>Introducing NotaMesa</title>
 </head>
